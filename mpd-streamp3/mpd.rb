f1df7a127029c2094a437ef6f5e6395b9a4eebd2
sam0402 <sam0402@gmail.com>
2025-08-21 21:50:09 +0800

    Add files via upload

---
+++ mpd-streamp3/mpd.rb
@@ -0,0 +1,209 @@
+class Mpd < Formula
+  desc "Music Player Daemon"
+  homepage "https://www.musicpd.org/"
+  url "https://github.com/MusicPlayerDaemon/MPD/archive/refs/tags/v0.24.5.tar.gz"
+  sha256 "ab7f9a2315eff97cc47dff5da9b9c675b1764cd50f161dc295b8a0d1956a6244"
+  license "GPL-2.0-or-later"
+  head "https://github.com/MusicPlayerDaemon/MPD.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any, arm64_sequoia: "7df4c2b8585a77146306286a4cef2810b773910e7499709688abf50b387b5507"
+    sha256 cellar: :any, arm64_sonoma:  "32fbf2daddfe4aec37f741ae11df5eda70d218fc9e588f9c65ebafab40cf308a"
+    sha256 cellar: :any, arm64_ventura: "4abae3ac8d154b1e41b1efc517da3eedeef757daece3bc0ae37c5d5f4f3bcf83"
+    sha256 cellar: :any, sonoma:        "5f8986c042cd0bd90e7dfb5756c0ba3fc026a510af28b3d5afc8351f169b11ce"
+    sha256 cellar: :any, ventura:       "66c3566557643d001e833b519e4b26b52a3ed67b5a68bd1d1195927d71bad912"
+    sha256               arm64_linux:   "3ff63b1bd9b995f116955aea067abbd6b9444c87fdb5a9d953a6596c20575965"
+    sha256               x86_64_linux:  "a4770ea4fb7b297bea1323a7c9bcba3b1c690ddbc5005ee7a6404a73e55c1c14"
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "nlohmann-json" => :build
+  depends_on "pkgconf" => :build
+
+  # depends_on "chromaprint"
+  depends_on "expat"
+  depends_on "faad2"
+  # depends_on "ffmpeg"
+  depends_on "flac"
+  # depends_on "fluid-synth"
+  depends_on "fmt"
+  # depends_on "game-music-emu"
+  # depends_on "icu4c@77"
+  depends_on "lame"
+  # depends_on "libao"
+  depends_on "libid3tag"
+  depends_on "libmad"
+  # depends_on "libmikmod"
+  depends_on "libmpdclient"
+  # depends_on "libnfs"
+  # depends_on "libogg"
+  # depends_on "libsamplerate"
+  # depends_on "libshout"
+  depends_on "libsndfile"
+  # depends_on "libsoxr"
+  # depends_on "libupnp"
+  # depends_on "libvorbis"
+  depends_on macos: :mojave # requires C++17 features unavailable in High Sierra
+  # depends_on "mpg123"
+  # depends_on "opus"
+  # depends_on "pcre2"
+  # depends_on "sqlite"
+  # depends_on "wavpack"
+
+  # uses_from_macos "bzip2"
+  uses_from_macos "curl"
+  uses_from_macos "zlib"
+
+  on_ventura :or_older do
+    depends_on "llvm"
+
+    fails_with :clang do
+      cause "Needs C++20 std::make_unique_for_overwrite"
+    end
+  end
+
+  on_linux do
+    depends_on "systemd" => :build
+    depends_on "alsa-lib"
+    depends_on "dbus"
+    # depends_on "jack"
+    # depends_on "pulseaudio"
+    # depends_on "systemd"
+  end
+
+  def install
+    if OS.mac? && MacOS.version <= :ventura
+      ENV.llvm_clang
+      ENV.append "LDFLAGS", "-L#{Formula["llvm"].opt_lib}/unwind -lunwind"
+      # When using Homebrew's superenv shims, we need to use HOMEBREW_LIBRARY_PATHS
+      # rather than LDFLAGS for libc++ in order to correctly link to LLVM's libc++.
+      ENV.prepend_path "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib/"c++"
+    end
+
+    args = %W[
+      --sysconfdir=#{etc}
+      -Dinotify=false
+      -Ddaemon=false
+      -Ddsd=false
+      -Dupnp=disabled
+      -Dlibmpdclient=disabled
+      -Dudisks=disabled
+      -Dwebdav=disabled
+      -Dcdio_paranoia=disabled
+      -Dcurl=enabled
+      -Dnfs=disabled
+      -Dsmbclient=disabled
+      -Dqobuz=disabled
+      -Dzzip=disabled
+      -Dbzip2=disabled
+      -Diso9660=disabled
+      -Did3tag=enabled
+      -Dchromaprint=disabled
+      -Dadplug=disabled
+      -Daudiofile=disabled
+      -Dfaad=enabled
+      -Dffmpeg=disabled
+      -Dflac=enabled
+      -Dfluidsynth=disabled
+      -Dnfs=disabled
+      -Dshout=disabled
+      -Dvorbisenc=disabled
+      -Dwavpack=disabled
+      -Dgme=disabled
+      -Dmad=enabled
+      -Dmikmod=disabled
+      -Dmodplug=disabled
+      -Dmpcdec=disabled
+      -Dmpg123=disabled
+      -Dopus=disabled
+      -Dsidplay=disabled
+      -Dsndfile=enabled
+      -Dtremor=disabled
+      -Dvorbis=disabled
+      -Dwavpack=disabled
+      -Dwildmidi=disabled
+      -Dvorbisenc=disabled
+      -Dlame=enabled
+      -Dtwolame=disabled
+      -Dshine=disabled
+      -Dlibsamplerate=disabled
+      -Dsoxr=disabled
+      -Dalsa=disabled
+      -Dao=disabled
+      -Dhttpd=true
+      -Djack=disabled
+      -Dopenal=disabled
+      -Doss=disabled
+      -Dpipe=false
+      -Dpipewire=disabled
+      -Dpulse=disabled
+      -Drecorder=false
+      -Dshout=disabled
+      -Dsnapcast=false
+      -Dsndio=disabled
+      -Dsolaris_output=disabled
+      -Dexpat=enabled
+      -Diconv=disabled
+      -Dpcre=disabled
+      -Dsqlite=disabled
+      -Dopenmpt=disabled
+      -Dnlohmann_json=enabled
+      -Dsystemd_system_unit_dir=#{lib}/systemd/system
+      -Dsystemd_user_unit_dir=#{lib}/systemd/user
+    ]
+
+    system "meson", "setup", "output/release", *args, *std_meson_args
+    system "meson", "compile", "-C", "output/release", "--verbose"
+    ENV.deparallelize # Directories are created in parallel, so let's not do that
+    system "meson", "install", "-C", "output/release"
+
+    pkgetc.install "doc/mpdconf.example" => "mpd.conf"
+  end
+
+  def caveats
+    <<~EOS
+      MPD requires a config file to start.
+      Please copy it from #{etc}/mpd/mpd.conf into one of these paths:
+        - ~/.mpd/mpd.conf
+        - ~/.mpdconf
+      and tailor it to your needs.
+    EOS
+  end
+
+  service do
+    run [opt_bin/"mpd", "--no-daemon"]
+    keep_alive true
+    process_type :interactive
+    working_dir HOMEBREW_PREFIX
+  end
+
+  test do
+    # oss_output: Error opening OSS device "/dev/dsp": No such file or directory
+    # oss_output: Error opening OSS device "/dev/sound/dsp": No such file or directory
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    assert_match "[wavpack] wv", shell_output("#{bin}/mpd --version")
+
+    require "expect"
+
+    port = free_port
+
+    (testpath/"mpd.conf").write <<~EOS
+      bind_to_address "127.0.0.1"
+      port "#{port}"
+    EOS
+
+    io = IO.popen("#{bin}/mpd --stdout --no-daemon #{testpath}/mpd.conf 2>&1", "r")
+    io.expect("output: Successfully detected a osx audio device", 30)
+
+    ohai "Connect to MPD command (localhost:#{port})"
+    TCPSocket.open("localhost", port) do |sock|
+      assert_match "OK MPD", sock.gets
+      ohai "Ping server"
+      sock.puts("ping")
+      assert_match "OK", sock.gets
+      sock.close
+    end
+  end
+end
