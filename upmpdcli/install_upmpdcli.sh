0d7ddd65b62fceab8c30afb22ddd7f05f79204f3
sam0402 <sam0402@gmail.com>
2025-11-18 13:40:02 +0800

    Update install_upmpdcli.sh

--- upmpdcli/install_upmpdcli.sh
+++ upmpdcli/install_upmpdcli.sh
@@ -25,8 +25,6 @@ echo "🎵 Installing upmpdcli ..."
 curl -fsSL https://raw.githubusercontent.com/sam0402/Homebrew/refs/heads/main/upmpdcli/upmpdcli-1.9.7.tar.gz | tar -xzf - -C /opt
 curl -fsSL https://raw.githubusercontent.com/sam0402/Homebrew/refs/heads/main/upmpdcli/upmpdcli.conf-xml -o /opt/homebrew/share/upmpdcli/upmpdcli.conf-xml
 curl -fsSL https://raw.githubusercontent.com/sam0402/Homebrew/refs/heads/main/upmpdcli/upmpdcli-config.tar.gz | tar -xzf - -C $HOME/Applications
-ln -sf /opt/homebrew/bin/upmpdcli-config.app/Contents/MacOS/upmpdcli-config /opt/homebrew/bin/upmpdcli-config
-
 
 echo "📁 Creating upmpdcli configuration folder..."
 if ! [ -f $HOME/.mpd/upmpdcli.conf ]; then
