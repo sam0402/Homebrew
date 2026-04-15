1eeffce6108a0ea53cd4a80a9a66d173028ff3bf
sam0402 <sam0402@gmail.com>
2026-04-15 22:02:24 +0800

    Add files via upload

---
+++ squeezelite/install_squeezelite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "🎵 Installing MPD ..."
+sudo curl -fsSL https://raw.githubusercontent.com/sam0402/Homebrew/refs/heads/main/squeezelite/squeezelite -o /Applications/squeezelite
+sudo chmod +x /Applications/squeezelite
+
+# Download and configure LaunchAgent plist
+curl -fsSL https://raw.githubusercontent.com/sam0402/Homebrew/refs/heads/main/squeezelite/com.squeezelite.start.plist -o ~/Library/LaunchAgents/com.squeezelite.start.plist
+
+# Load and start the LaunchAgent
+launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/com.squeezelite.start.plist
+launchctl enable gui/$(id -u)/com.squeezelite.start
+launchctl kickstart -k gui/$(id -u)/com.squeezelite.start
+
+echo "✅ squeezelite installation and setup complete."
