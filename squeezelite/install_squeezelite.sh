2b991ca440ba0173a7132abd4a2fd3ec54b4a372
sam0402 <sam0402@gmail.com>
2026-05-11 19:39:44 +0800

    Fix homebrew install command syntax

--- squeezelite/install_squeezelite.sh
+++ squeezelite/install_squeezelite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "🎵 Installing MPD ..."
-homebrew install flac
+brew install flac
 sudo curl -fsSL https://raw.githubusercontent.com/sam0402/Homebrew/refs/heads/main/squeezelite/squeezelite -o /Applications/squeezelite
 sudo chmod +x /Applications/squeezelite
 
